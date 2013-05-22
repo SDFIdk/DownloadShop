3310a048bb397d86464cc710c89ad32e37569e08
Mikkel Jakobsen <mikkel@adapt.dk>
2013-05-22 23:06:31 +0000

    FIxing UI glitches in subuser module

--- htdocs/sites/all/modules/custom/kms_subuser/js/subuser_popup.js
+++ htdocs/sites/all/modules/custom/kms_subuser/js/subuser_popup.js
@@ -2,13 +2,41 @@
   var result = 0, prevResult = 0;
   Drupal.behaviors.kmsSubuserPopup = {
     attach: function(context, settings) {
+      // Some external script is injecting divs with css files in the head.
+      // Lets get rid of them:
+      $('head div').remove();
 
+      // Load subuser view.
+      $('#subuser-view-wrapper').subuserView(settings.kms_user.uid);
+
+      // If number of view result is increasing then clear form.
+      $("body").bind("ajaxComplete", function(e, xhr, sett){
+          Drupal.behaviors.ZZCToolsModal.attach(context);
+          result = $('#subuser-view-wrapper table td.views-field-name').length;
+          if (prevResult != 0 && result != prevResult) {
+            $('#kms-subuser-form input[type="text"]').val('');
+            $('#kms-subuser-form textarea').text('');
+            $('#kms-subuser-form .form-item-name').hide();
+            $('#kms-subuser-form .form-item-pass').hide();
+            $('#kms-subuser-form .form-item-user-expire').hide();
+            $('#kms-subuser-form .form-item-user-remark').hide();
+            $('#kms-subuser-form .form-submit').hide();
+            $('#kms-subuser-form input[name="add_subuser"]').removeAttr('checked');
+          }
+          prevResult = result;
+      });
+    }
+  }
+
+  $.fn.subuserView = function (uid) {
+    return this.each(function() {
+      el = $(this);
       $.ajax({
-        url: '/kms-subuser/ajax/load-view/' + settings.kms_user.uid,
+        url: '/kms-subuser/ajax/load-view/' + uid,
         type: 'GET',
         success: function(xhr) {
-          $('#subuser-view-wrapper').html(xhr);
-          $('#subuser-view-wrapper table').dataTable({
+          el.html(xhr);
+          el.find('table').dataTable({
             "bPaginate": false,
             "oLanguage": {
               "sSearch": Drupal.t('Search'),
@@ -17,23 +45,12 @@
             }
           });
            
-          // If number of view result is increasing then clear form.
-          $("body").bind("ajaxComplete", function(e, xhr, sett){
-              Drupal.behaviors.ZZCToolsModal.attach(context);
-              result = $('#subuser-view-wrapper table td.views-field-name').length;
-              if (prevResult != 0 && result > prevResult) {
-                $('#kms-subuser-form input[type="text"]').val('');
-                $('#kms-subuser-form textarea').text('');
-                $('#kms-subuser-form input[name="add_subuser"]')
-                 .trigger('click').removeAttr('checked');
-              }
-              prevResult = result;
-          });
-
         }
-      });
-      
-    }
+      });      
+    });
+    
   }
 
 })(jQuery, Drupal);
+
+
