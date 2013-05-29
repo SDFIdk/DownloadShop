64dddf50439070b61bd2c6a3294502054d7fed9f
Mikkel Jakobsen <mikkel@adapt.dk>
2013-05-29 10:05:11 +0000

    Geo Data 35 - Tilretninger til Subuser implementeringen  https://iw2.i.adapt.dk/support/support/13190/ DONE

--- htdocs/sites/all/modules/custom/kms_subuser/js/subuser_popup.js
+++ htdocs/sites/all/modules/custom/kms_subuser/js/subuser_popup.js
@@ -8,23 +8,6 @@
 
       // Load subuser view.
       $('#subuser-view-wrapper').subuserView(settings.kms_user.uid);
-
-      // If number of view result is increasing then clear form.
-      $("body").bind("ajaxComplete", function(e, xhr, sett){
-          Drupal.behaviors.ZZCToolsModal.attach(context);
-          result = $('#subuser-view-wrapper table td.views-field-name').length;
-          if (prevResult != 0 && result != prevResult) {
-            $('#kms-subuser-form input[type="text"]').val('');
-            $('#kms-subuser-form textarea').text('');
-            $('#kms-subuser-form .form-item-name').hide();
-            $('#kms-subuser-form .form-item-pass').hide();
-            $('#kms-subuser-form .form-item-user-expire').hide();
-            $('#kms-subuser-form .form-item-user-remark').hide();
-            $('#kms-subuser-form .form-submit').hide();
-            $('#kms-subuser-form input[name="add_subuser"]').removeAttr('checked');
-          }
-          prevResult = result;
-      });
     }
   }
 
