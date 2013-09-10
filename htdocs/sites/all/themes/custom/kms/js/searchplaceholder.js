80de5262658158b2aa7529728b7ef46cdbacfa2a
Tom Helmer Hansen <tom@adapt.dk>
2013-09-10 07:21:06 +0000

    map search js updated

--- htdocs/sites/all/themes/custom/kms/js/searchplaceholder.js
+++ htdocs/sites/all/themes/custom/kms/js/searchplaceholder.js
@@ -1,5 +1,8 @@
 jQuery(document).ready(function($) {
     // DA
-  $(".map-widget input#geosearch").attr("placeholder", "Søg her");
-
+  $(".pane-kms-mapwidget-pane .ui-widget a.search").click(function() {
+    $(".pane-kms-mapwidget-pane .ui-widget").toggleClass("show-search");
+  });
+  
+  $(".pane-kms-mapwidget-pane input#geosearch").attr("placeholder", "Søg");
 });
