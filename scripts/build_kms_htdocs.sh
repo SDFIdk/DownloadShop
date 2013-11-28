a6f791dac5add2a7eb9e2f9492b39072886db1e0
Mikkel Jakobsen <mikkel@adapt.dk>
2013-11-28 21:55:06 +0000

    Putting build script where it belongs. Now it builds htdocs

---
+++ scripts/build_kms_htdocs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+PROJECT="kms.dev"
+SETTINGS_FILE="mikkel.settings.php"
+REPO="svn+ssh://svnhost/data/subversion/drupal/kms/trunk/htdocs"
+
+BASE_PATH=$(echo `pwd`);
+
+TMP_ID=$(echo `date +%s`)
+TMP_DIR="/tmp/${PROJECT}_${TMP_ID}"
+TMP_SVN_DIR="${TMP_DIR}/svn"
+TMP_MAKE_DIR="${TMP_DIR}/make"
+
+HTDOCS="${TMP_SVN_DIR}/htdocs"
+SITES_DEFAULT="${HTDOCS}/sites/default"
+PROFILE="${HTDOCS}/profiles/kms"
+
+svn co $REPO $HTDOCS
+rm "${SITES_DEFAULT}/settings.php"
+cd $SITES_DEFAULT && ln -s $SETTINGS_FILE settings.php
+
+drush make "${PROFILE}/kms.start.make" $TMP_MAKE_DIR
+
+rm -rf "${BASE_PATH}/htdocs" 2>/dev/null
+mkdir -p "${BASE_PATH}/htdocs" \
+&& cp -rp "${TMP_MAKE_DIR}/." "${BASE_PATH}/htdocs/" \
+&& cp -rp "${HTDOCS}/." "${BASE_PATH}/htdocs/" \
+&& rm -rf $YMP_DIR \
+&& echo -e "\nDone building ${PROJECT} htdocs...\n"
+
