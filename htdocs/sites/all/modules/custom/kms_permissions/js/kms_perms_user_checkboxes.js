007868e409b1477754db8676b5b2441d3bf13375
Mikkel Jakobsen <mikkel@adapt.dk>
2013-12-04 22:39:11 +0000

    Added copy services VBO and refined kms_perms_user_checkboxes.js

--- htdocs/sites/all/modules/custom/kms_permissions/js/kms_perms_user_checkboxes.js
+++ htdocs/sites/all/modules/custom/kms_permissions/js/kms_perms_user_checkboxes.js
@@ -5,13 +5,14 @@
 (function ($) {
   Drupal.behaviors.kms_perms_user_checkboxes = {
     attach: function(context, settings) {
+      // Atodo: handle individual permissions.
       $('#edit-field-access-bundles-und .form-type-checkbox input', context).click(function () {
-
         var bids = [];
         $('#edit-field-access-bundles-und .form-type-checkbox input:checked', context).each(function(){
           bids.push($(this).val())
         })
 
+        $('.group-webservices .form-type-checkbox input').attr("checked", false).unbind('click.kmsPermissionJail');
         $('.group-webservices').foggy();
         $.ajax({
           url: '/kms-permissions/ajax/load-sids-by-bid',
@@ -24,14 +25,18 @@
             for (var i=0; i < xhr.sids.length; i++){
               var sid = xhr.sids[i];
               var selector = '.group-webservices .form-type-checkbox input[id^="edit-field-bundle-webservices-"][id$=-' + sid + ']'
-              $(selector, context).attr("checked", "checked").attr("disabled", "disabled");
+              $(selector, context).attr("checked", "checked");
+              $(selector, context).bind('click.kmsPermissionJail', function(event){
+                event.stopPropagation();
+                event.preventDefault();
+                return false;
+              });
             }
           },
           complete: function() {
             $('.group-webservices').foggy(false);
           }
         });
-
       })
 
     }
