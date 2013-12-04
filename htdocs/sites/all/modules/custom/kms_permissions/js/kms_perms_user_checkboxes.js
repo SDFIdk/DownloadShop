21ae0e2b4be4bcd1881f6d9863ae0652a2941311
Mikkel Jakobsen <mikkel@adapt.dk>
2013-12-04 23:11:05 +0000

    fixing individual services

--- htdocs/sites/all/modules/custom/kms_permissions/js/kms_perms_user_checkboxes.js
+++ htdocs/sites/all/modules/custom/kms_permissions/js/kms_perms_user_checkboxes.js
@@ -5,23 +5,32 @@
 (function ($) {
   Drupal.behaviors.kms_perms_user_checkboxes = {
     attach: function(context, settings) {
-      // Atodo: handle individual permissions.
+      sids = [];
       $('#edit-field-access-bundles-und .form-type-checkbox input', context).click(function () {
+
         var bids = [];
         $('#edit-field-access-bundles-und .form-type-checkbox input:checked', context).each(function(){
           bids.push($(this).val())
         })
 
-        $('.group-webservices .form-type-checkbox input').attr("checked", false).unbind('click.kmsPermissionJail');
         $('.group-webservices').foggy();
         $.ajax({
           url: '/kms-permissions/ajax/load-sids-by-bid',
           type: 'POST',
           'data': {bids: bids},
           success: function(xhr) {
-            $('.group-webservices .form-type-checkbox input', context)
-              .attr('checked', false)
-              .attr('disabled', false);
+
+            $('.group-webservices .form-type-checkbox input').each(function(){
+              $(this).unbind('click.kmsPermissionJail');
+              console.log(sids.indexOf($(this).val()));
+              console.log($(this).val());
+              if(sids.indexOf($(this).val()) != -1) {
+                $(this).attr("checked", false);
+              }
+            });
+
+            sids = xhr.sids;
+
             for (var i=0; i < xhr.sids.length; i++){
               var sid = xhr.sids[i];
               var selector = '.group-webservices .form-type-checkbox input[id^="edit-field-bundle-webservices-"][id$=-' + sid + ']'
@@ -32,6 +41,7 @@
                 return false;
               });
             }
+            
           },
           complete: function() {
             $('.group-webservices').foggy(false);
