61c1a33c6977da6e02f296976abba7bdde7ae5f2
Mikkel Jakobsen <mikkel@adapt.dk>
2013-12-05 23:06:31 +0000

    kms_oci_queue_reports ui improvements

--- htdocs/sites/all/modules/custom/kms_oci_queue/js/kms_oci_queue_reports.js
+++ htdocs/sites/all/modules/custom/kms_oci_queue/js/kms_oci_queue_reports.js
@@ -4,19 +4,23 @@
       $('td.job-log-trigger', context).click(function(){
         var jobId = $(this).attr("data-jid");
         var currentTd = $(this);
-        $.ajax({
-          url: '/kms-oci-queue/ajax/load-log/' + jobId,
-          type: 'GET',
-          success: function(xhr) {
-            $('.kms-oci-queue-job-log', context).not('[data-jid="' + jobId + '"]').hide();
-            $('.kms-oci-queue-job-log[data-jid="' + jobId + '"]', context).show();
-            $('.kms-oci-queue-job-log[data-jid="' + jobId + '"] td', context).html(xhr.html);
-            $('td.job-log-trigger', context).not(currentTd).removeClass('active');
-            currentTd.addClass('active');
-            console.log(currentTd);
-
-          }
-        });
+        if(currentTd.hasClass('active')) {
+          $('.kms-oci-queue-job-log[data-jid="' + jobId + '"]', context).hide();
+          currentTd.removeClass('active');
+        }
+        else {
+          $.ajax({
+            url: '/kms-oci-queue/ajax/load-log/' + jobId,
+            type: 'GET',
+            success: function(xhr) {
+              $('.kms-oci-queue-job-log', context).not('[data-jid="' + jobId + '"]').hide();
+              $('.kms-oci-queue-job-log[data-jid="' + jobId + '"]', context).show();
+              $('.kms-oci-queue-job-log[data-jid="' + jobId + '"] td', context).html(xhr.html);
+              $('td.job-log-trigger', context).not(currentTd).removeClass('active');
+              currentTd.addClass('active');
+            }
+          });
+        }
       });
     }
   }
