f8a5f172a0b73c09e168208a8d83c15c985863f8
Mikkel Jakobsen <mikkel@adapt.dk>
2014-01-24 13:19:18 +0000

    query engine is ready to be run by cron

--- scripts/query_engine/query_engine.php
+++ scripts/query_engine/query_engine.php
@@ -2,12 +2,15 @@
 /**
  * @file
  * query_engine.php.
+ * Used by the kms_oci_queue drupal module for oracle query execution.
+ *
  * User: mikkel@adapt.dk
  * Date: 01/21/14 - 11:16 PM
  */
 
 // Define paths.
 define('QUERY_ENGINE_ROOT', dirname(__FILE__));
+define('QUERY_ENGINE_RUN_MODE', 'cron');
 define('DRUPAL_ROOT', realpath(QUERY_ENGINE_ROOT . '/../../htdocs'));
 
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
@@ -24,15 +27,31 @@ define(
   sprintf('%s/sites/default/files/%s', DRUPAL_ROOT, KMS_OCI_QUEUE_PREFIX)
 );
 
-// Run as admin.
-$user = user_load(1);
-// Create arguments from given script options.
-$args = qe_resolve_arguments();
-
 // If kms_oci_queue is not enabled do nothing.
 if (!module_exists('kms_oci_queue')) {
   qe_error('kms_oci_queue is not enabled');
 }
+
+// Run as admin.
+$user = user_load(1);
+
+switch (QUERY_ENGINE_RUN_MODE) {
+  case 'cron':
+    $opt = array();
+    // Get job filename from file_scan_directory of jobs dir.
+    $opt['f'] = qe_find_job_file_lowest_id();
+    $file = qe_absolute_filepath($opt['f']);
+    // Get job type from job file comment.
+    $opt['t'] = qe_get_job_type_from_file($file);
+    break;
+
+  case 'cli':
+    $opt = qe_set_options_from_cli();
+    break;
+}
+
+// Create arguments from given script options.
+$args = qe_resolve_arguments($opt);
 
 // Get the current job.
 $job = KmsOciQueueJobFactory::get($args['jid']);
@@ -59,8 +78,10 @@ $db_conf = array(
   'host' => $conf['kms_oci_conn_host'],
   'db' => $conf['kms_oci_conn_db'],
 );
+
 // Generate sql string.
 $sql = qe_generate_sql($db_conf, $args);
+
 // Report sql execution.
 $job->changeStatus(
   KmsOciQueueJob::STATUS_PROCESSING,
@@ -69,12 +90,13 @@ $job->changeStatus(
 );
 // Execute generated sql and get result of it.
 $result = qe_execute_sql($sql, $args);
+
 // Move file to processed folder upon success.
 if ($result['exit_code'] === 0) {
   // Move file.
   rename(
-    sprintf('%s/jobs/%s', KMS_OCI_QUEUE_ENGINE_ROOT, $args['filename']),
-    sprintf('%s/jobs_processed/%s', KMS_OCI_QUEUE_ENGINE_ROOT, $args['filename'])
+    qe_absolute_filepath($args['filename'], KMS_OCI_QUEUE_ENGINE_DIR_JOBS),
+    qe_absolute_filepath($args['filename'], KMS_OCI_QUEUE_ENGINE_DIR_JOBS_PROCESSED)
   );
   // Report everything well and change status to 'done'.
   $job->changeStatus(
