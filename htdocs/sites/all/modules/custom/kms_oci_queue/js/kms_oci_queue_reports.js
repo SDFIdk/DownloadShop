bf758eef48d8c9b9f2f91dfa40d920d2c5f93c2d
Mikkel Jakobsen <mikkel@adapt.dk>
2014-03-05 20:24:30 +0000

    Fixing kms oci report js + adding timers for debug

--- htdocs/sites/all/modules/custom/kms_oci_queue/js/kms_oci_queue_reports.js
+++ htdocs/sites/all/modules/custom/kms_oci_queue/js/kms_oci_queue_reports.js
@@ -3,33 +3,8 @@
     attach: function(context, settings) {
       // Poll job tables.
       pollJobs();
-
-      $('td.job-log-trigger', context).click(function(){
-        var jobId = $(this).attr("data-jid");
-        var currentTd = $(this);
-        if(currentTd.hasClass('active')) {
-          $('.kms-oci-queue-job-log[data-jid="' + jobId + '"]', context).hide();
-          currentTd.removeClass('active');
-        }
-        else {
-          currentTd.addClass('loading');
-          $.ajax({
-            url: '/kms-oci-queue/ajax/load-log/' + jobId,
-            type: 'GET',
-            success: function(xhr) {
-              $('.kms-oci-queue-job-log', context).not('[data-jid="' + jobId + '"]').hide();
-              $('.kms-oci-queue-job-log[data-jid="' + jobId + '"]', context).show();
-              $('.kms-oci-queue-job-log[data-jid="' + jobId + '"] td', context).html(xhr.html);
-              $('td.job-log-trigger', context).not(currentTd).removeClass('active');
-              currentTd.addClass('active');
-              $.cookie('kms-oci-queue-job-log-open-jid', jobId);
-            },
-            complete: function() {
-              currentTd.removeClass('loading');
-            }
-          });
-        }
-      });
+      jobLogTrigger();
+      $('a', context).bt({width: 300});
 
       // Update job tables with polling.
       function pollJobs() {
@@ -41,14 +16,43 @@
               if (xhr.stateChanged) {
                 $('.kms-oci-queue-jobs-overview', context).html(xhr.html);
                 $('.bt-wrapper').hide();
-                Drupal.attachBehaviors(context, settings);
-              }
+                jobLogTrigger();
+                $('a', context).bt({width: 300});              }
             },
             complete: pollJobs,
             timeout: 4500
           });
         }, 4500);
       }
+
+      function jobLogTrigger() {
+        $('td.job-log-trigger', context).click(function(){
+          var jobId = $(this).attr("data-jid");
+          var currentTd = $(this);
+          if(currentTd.hasClass('active')) {
+            $('.kms-oci-queue-job-log[data-jid="' + jobId + '"]', context).hide();
+            currentTd.removeClass('active');
+          }
+          else {
+            currentTd.addClass('loading');
+            $.ajax({
+              url: '/kms-oci-queue/ajax/load-log/' + jobId,
+              type: 'GET',
+              success: function(xhr) {
+                $('.kms-oci-queue-job-log', context).not('[data-jid="' + jobId + '"]').hide();
+                $('.kms-oci-queue-job-log[data-jid="' + jobId + '"]', context).show();
+                $('.kms-oci-queue-job-log[data-jid="' + jobId + '"] td', context).html(xhr.html);
+                $('td.job-log-trigger', context).not(currentTd).removeClass('active');
+                currentTd.addClass('active');
+              },
+              complete: function() {
+                currentTd.removeClass('loading');
+              }
+            });
+          }
+        });
+      }
+
     }
   }
 
