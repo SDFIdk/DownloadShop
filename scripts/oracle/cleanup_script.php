a43b9069b1c31bf42974008eb5dc1ae07cd15b90
Linas Balke <linas@adapt.dk>
2014-03-05 09:23:39 +0000

    GSTR 131, update cleanup and transfer script so they include applications

--- scripts/oracle/cleanup_script.php
+++ scripts/oracle/cleanup_script.php
@@ -7,57 +7,67 @@
  */
 $counter = 0;
 
-$service_types = kms_permissions_widget_type_options(); 
+// Load all webservice types
+$service_types = kms_permissions_widget_type_options();
 $type = 'geo';
 $count1 = db_query('SELECT COUNT(entity_id) FROM field_data_field_bundle_webservices_'.$type.' WHERE bundle =:bundle_type', array(':bundle_type'=> 'user'))->fetchField();
 Print(' Total records in separate service table on start '.$count1 . "\xA");
 
-
+// Webservices by type
 foreach ($service_types as $type) {
-
   $result = db_query('SELECT bundle.entity_id as userid, bundle.field_access_bundles_nid, webservices.entity_id, webservices.field_bundle_webservices_'.$type.'_value as serviceid FROM {field_data_field_access_bundles} as bundle
-    RIGHT JOIN field_data_field_bundle_webservices_'.$type.' as webservices 
+    RIGHT JOIN field_data_field_bundle_webservices_'.$type.' as webservices
     ON bundle.field_access_bundles_nid = webservices.entity_id
-    WHERE webservices.bundle =:bundle_type
-    AND bundle.entity_id = :entity_id', array(':bundle_type' => 'access_bundle', ':entity_id' => '4715'));
-  
-  foreach ($result as $record) {   
-    
-   // print_r($record);
+    WHERE webservices.bundle =:bundle_type', array(':bundle_type' => 'access_bundle'));
+  foreach ($result as $record) {
     $counter++;
     remove_record($record->userid, $type, $record->serviceid);
   }
-
-
 }
 
-/* Test particular user by uid
-print(' User 4715 has these separatate permissions in permissions table ' . "\xA");
+// Applications
+$result_app = db_query('SELECT bundle.entity_id as userid, bundle.field_access_bundles_nid, applications.entity_id, applications.field_applications_value as serviceid FROM {field_data_field_access_bundles} as bundle
+  RIGHT JOIN field_data_field_applications as applications
+  ON bundle.field_access_bundles_nid = applications.entity_id
+  WHERE applications.bundle =:bundle_type', array(':bundle_type' => 'access_bundle'));
 
-$type = 'geo';
+foreach ($result_app as $record) {
+  $counter++;
+  remove_record_application($record->userid, $record->serviceid);
+}
 
-$result = db_query('SELECT field_bundle_webservices_'.$type.'_value
-    FROM field_data_field_bundle_webservices_'.$type.' 
-    WHERE bundle =:bundle_type
-    AND entity_id = :entity_id', array(':bundle_type' => 'user', ':entity_id' => '4715'));
-
-  foreach ($result as $record) {   
-    print_r($record);
-  }
-
-*/
-
+// Some output information after script run finalized
 print(' Found '.$counter.' rows in Bundle sets '. "\xA");
 
 $count2 = db_query('SELECT COUNT(entity_id) FROM field_data_field_bundle_webservices_'.$type.' WHERE bundle =:bundle_type', array(':bundle_type'=> 'user'))->fetchField();
 Print(' Total records in separate service table after '.$count2 .' removed '. ($count1 - $count2) . "\xA");
 
-
-
+/**
+* Remove overlapping permissions from separate permission table
+* @param $userid
+*   User identifier who's permissions will be removed.
+* @param $type
+*   Type of the webservice
+* @param $serviceid
+*   Service identifier
+*/
 function remove_record($userid, $type, $serviceid){
-  //print('Removed '.$userid. ' with service_id '.$serviceid.  "\xA");
-  db_query('DELETE FROM {field_data_field_bundle_webservices_'.$type.'} 
+  db_query('DELETE FROM {field_data_field_bundle_webservices_'.$type.'}
     WHERE entity_id = :entity_id AND field_bundle_webservices_'.$type.'_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
-  db_query('DELETE FROM {field_revision_field_bundle_webservices_'.$type.'} 
+  db_query('DELETE FROM {field_revision_field_bundle_webservices_'.$type.'}
     WHERE entity_id = :entity_id AND field_bundle_webservices_'.$type.'_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
+}
+
+/**
+* Remove overlapping permissions from separate permission table
+* @param $userid
+*   User identifier who's permissions will be removed.
+* @param $serviceid
+*   Service identifier
+*/
+function remove_record_application($userid, $serviceid){
+  db_query('DELETE FROM {field_data_field_applications}
+    WHERE entity_id = :entity_id AND field_applications_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
+  db_query('DELETE FROM {field_revision_field_applications}
+    WHERE entity_id = :entity_id AND field_applications_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
 }
--- scripts/oracle/transfer_script.php
+++ scripts/oracle/transfer_script.php
@@ -11,17 +11,18 @@ $settings = array(
 
 $counter = 0;
 
+// USERRESTRICTIONS2 - ALL Webservices
 if (kms_oci_get_conn('kms_permissions', $settings)) {
 
   $query = "SELECT * from (
-  	SELECT US2.USERID, S2.SERVICEID, S2.SERVICENAME, ST.TYPENAME 
+  	SELECT US2.USERID, S2.SERVICEID, S2.SERVICENAME, ST.TYPENAME
   	From USERRESTRICTIONS2 US2
-  	LEFT JOIN SERVICES2 S2 
+  	LEFT JOIN SERVICES2 S2
   	ON US2.SERVICEID = S2.SERVICEID
     LEFT JOIN SERVICETYPES2 ST
     ON S2.TYPEID = ST.TYPEID
-    ) 
-where ROWNUM <=200000";  
+    )
+where ROWNUM <=200000";
 
 $rows = kms_oci_select($query, array(), 'kms_permissions');
 
@@ -29,11 +30,9 @@ if (!empty($rows)) {
   foreach ($rows as $row) {
     $userid = get_drupal_uid($row['USERID']);
     $type = strtolower($row['TYPENAME']);
-
     if(!is_null($userid) && $type != "tms"){
-
         // Remove all associated service records with this user in this type of table, we gonna rewrite them with Oracle
-      remove_duplicate($userid,$type,$row['SERVICEID']);
+      remove_webservice_duplicate($userid,$type,$row['SERVICEID']);
 
       $fields = array(
        'entity_type' => 'user',
@@ -44,7 +43,7 @@ if (!empty($rows)) {
        'language' => LANGUAGE_NONE,
        'delta' => get_next_delta($userid, $type),
        'field_bundle_webservices_'.$type.'_value' => $row['SERVICEID']
-       );    
+       );
 
       db_insert('field_data_field_bundle_webservices_'.$type)
       ->fields($fields)
@@ -53,21 +52,65 @@ if (!empty($rows)) {
       db_insert('field_revision_field_bundle_webservices_'.$type)
       ->fields($fields)
       ->execute();
-
       $counter++;
-
     }
-
   }
-
 }
 
 kms_oci_close_conn('kms_permissions');
 }
 
-print('Script successfully inserted/updated '.$counter.' rows ');
+// SMSUSERGROUP -  Applications
+if (kms_oci_get_conn()) {
+  //Delete existing permissions for user
+  $query = "SELECT * FROM SMUSERGROUP";
+  $rows = kms_oci_select($query, array());
+  if (!empty($rows)) {
+    foreach ($rows as $row) {
+      $userid = get_drupal_uid($row['USERID']);
+      print('User id '.$userid.' will be altered with permissions from application '.$row['GROUPID'].' \n');
+      if(!is_null($userid)){
+
+        remove_application_duplicate($userid,$row['GROUPID']);
+
+        $fields = array(
+       'entity_type' => 'user',
+       'bundle' => 'user',
+       'deleted' => 0,
+       'entity_id' => $userid,
+       'revision_id' => $userid,
+       'language' => LANGUAGE_NONE,
+       'delta' => get_next_delta_applications($userid),
+       'field_applications_value' => $row['GROUPID']
+       );
+
+      db_insert('field_data_field_applications')
+      ->fields($fields)
+      ->execute();
+
+      db_insert('field_revision_field_applications')
+      ->fields($fields)
+      ->execute();
+
+        $counter++;
+      }
+    }
+  }
+
+  // Close connection
+  kms_oci_close_conn();
+}
+
+print('Script successfully inserted/updated from webservices'.$counter.' rows ');
 
 
+/**
+* Get drupal User idenfier
+* @param $kms_user_id
+*   ID returned from oracle, might be old or new (drupal) format
+* @return $uid
+*   UID
+*/
 function get_drupal_uid($kms_user_id){
 
   if(is_numeric($kms_user_id) && strlen($kms_user_id) < 7) {
@@ -77,9 +120,9 @@ function get_drupal_uid($kms_user_id){
     $query = db_select('field_data_field_kms_user_id', 'kms')
     ->condition('field_kms_user_id_value', $kms_user_id, '=')
     ->fields('kms', array('entity_id'));
-    
+
     $result = $query->execute();
-    
+
     while($record = $result->fetchAssoc()) {
       if(!empty($record['entity_id'])) {
        return $record['entity_id'];
@@ -90,16 +133,58 @@ function get_drupal_uid($kms_user_id){
   }
 }
 
+/**
+* Get next delta, so one user can have more than one separate service correctly inserted
+* @param $userid
+*   User identifier who's permissions will be removed.
+* @param $type
+*   Type of the service
+*/
 function get_next_delta($userid, $type){
   $last_delta = db_query('SELECT MAX(delta) FROM {field_data_field_bundle_webservices_'.$type.'} WHERE entity_id = '.$userid.'')
   ->fetchField();
-  
+
   return $last_delta != "" ? $last_delta+1:0;
 }
 
-function remove_duplicate($userid, $type, $serviceid){
-  db_query('DELETE FROM {field_data_field_bundle_webservices_'.$type.'} 
+/**
+* Get next delta for applications, so one user can have more than one separate service correctly inserted
+* @param $userid
+*   User identifier who's permissions will be removed.
+*/
+function get_next_delta_applications($userid){
+  $last_delta = db_query('SELECT MAX(delta) FROM {field_data_field_applications} WHERE entity_id = '.$userid.'')
+  ->fetchField();
+
+  return $last_delta != "" ? $last_delta+1:0;
+}
+
+/**
+* Remove all permissions before inserting from Oracle
+* @param $userid
+*   User identifier who's permissions will be removed.
+* @param $type
+*   Type of the service
+* @param $serviceid
+*   Service identifier
+*/
+function remove_webservice_duplicate($userid, $type, $serviceid){
+  db_query('DELETE FROM {field_data_field_bundle_webservices_'.$type.'}
     WHERE entity_id = :entity_id AND field_bundle_webservices_'.$type.'_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
-  db_query('DELETE FROM {field_revision_field_bundle_webservices_'.$type.'} 
+  db_query('DELETE FROM {field_revision_field_bundle_webservices_'.$type.'}
     WHERE entity_id = :entity_id AND field_bundle_webservices_'.$type.'_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
+}
+
+/**
+* Remove all permissions before inserting from Oracle for applications
+* @param $userid
+*   User identifier who's permissions will be removed.
+* @param $serviceid
+*   Service identifier
+*/
+function remove_application_duplicate($userid, $serviceid){
+  db_query('DELETE FROM {field_data_field_applications}
+    WHERE entity_id = :entity_id AND field_applications_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
+  db_query('DELETE FROM {field_revision_field_applications}
+    WHERE entity_id = :entity_id AND field_applications_value = :webservice_id', array(':entity_id' => $userid, ':webservice_id' => $serviceid));
 }
