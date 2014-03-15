39c23e685f29887664c98367636fb98006b2a3eb
Mikkel Jakobsen <mikkel@adapt.dk>
2014-03-15 20:48:44 +0000

    Job group implemented in query engine

--- scripts/query_engine/query_engine.php
+++ scripts/query_engine/query_engine.php
@@ -46,7 +46,6 @@ switch (QUERY_ENGINE_RUN_MODE) {
     $opt = array();
     // Get job filename from file_scan_directory of jobs dir.
     $opt['f'] = qe_find_job_file_lowest_id();
-    $file = qe_absolute_filepath($opt['f']);
     break;
 
   case 'cli':
@@ -54,82 +53,34 @@ switch (QUERY_ENGINE_RUN_MODE) {
     break;
 }
 
-// Create arguments from given script options.
-$args = qe_resolve_arguments($opt);
-
 // Get the current job.
 $job = KmsOciQueueJobFactory::get($args['jid']);
 
-// If job could not be loaded then exit.
-if (!$job) {
-  qe_error('No jobs existing with id: %jid', array('%jid' => $args['jid']));
+// If job is a standalone job only process that one.
+// Or if the run mode is cli then only process one job at a time.
+if (!$job->gid || QUERY_ENGINE_RUN_MODE == 'cli') {
+  // Create arguments from given script options.
+  $args = qe_resolve_arguments($opt);
+  // Check if the state of the job is valid.
+  qe_job_validate_state($job, $args);
+  // Go for it.
+  qe_process_job($job, $args, $opt);
 }
-
-// Only process job if it has an valid status.
-if (!qe_job_has_valid_status($job)) {
-  exit(1);
-}
-
-// If there are undone jobs before this one do not import it yet.
-if (qe_undone_jobs_before_current_exists($job)) {
-  exit(1);
-}
-
-// If the job file could not be loaded change job status and exit.
-if (!file_exists($args['filepath'])) {
-  $message = 'Query engine: File: %file does not exist';
-  $vars = array('%file' => $args['filepath']);
-  $job->changeStatus(KmsOciQueueJob::STATUS_FAILED, $message, $vars);
-  qe_error($message, $vars, TRUE);
-}
-
-// Db connection settings.
-$db_conf = KmsOciQueueJobDb::getConnectionSettings($job->cid);
-
-// Get job info from job action if not specified.
-$args['info'] = !empty($opt['i']) ? $opt['i'] : $job->action . ' | ' . $job->actionDetails;
-
-// Generate sql string.
-$sql = qe_generate_sql($db_conf, $args);
-
-// Report sql execution.
-$job->changeStatus(
-  KmsOciQueueJob::STATUS_PROCESSING,
-  'Query engine: %jid is being processed',
-  array('%jid' => $job->jid)
-);
-// Execute generated sql and get result of it.
-$result = qe_execute_sql($sql, $args);
-
-// Move file to processed folder upon success.
-if ($result['exit_code'] === 0) {
-  // Move file.
-  rename(
-    qe_absolute_filepath($args['filename'], KMS_OCI_QUEUE_ENGINE_DIR_JOBS),
-    qe_absolute_filepath($args['filename'], KMS_OCI_QUEUE_ENGINE_DIR_JOBS_PROCESSED)
-  );
-  // Report everything well and change status to 'done'.
-  $job_status = KmsOciQueueJob::STATUS_DONE;
-  $message = implode('; ', $result['message']);
-}
+// Otherwise process group of jobs.
 else {
-  $job_status = KmsOciQueueJob::STATUS_FAILED;
-  if (!empty($result['message'])) {
-    $message = implode('; ', $result['message']);
-    $message_vars = array();
-  }
-  else {
-    $message = 'Something went wrong with exit code: @exit_code.';
-    $message_vars = array('@exit_code' => $result['exit_code']);
+  $jids = KmsOciQueueJobGroup::getJidsFromGid($job->gid);
+  foreach ($jids as $jid) {
+    // Only process current job and latter ones in group.
+    if ($jid < $job->jid) {
+      continue;
+    }
+    $job = KmsOciQueueJobFactory::get($jid);
+    $opt['f'] = $job->jid . '.job';
+    // Create arguments from given script options.
+    $args = qe_resolve_arguments($opt);
+    // Check if the state of the job is valid.
+    qe_job_validate_state($job, $args);
+    // Go for it.
+    qe_process_job($job, $args, $opt);
   }
 }
-
-// Change job status depending on exit code.
-$job->changeStatus(
-  $job_status,
-  'Query engine: ' . $message,
-  $message_vars
-);
-
-// Expose exit code.
-exit($result['exit_code']);
