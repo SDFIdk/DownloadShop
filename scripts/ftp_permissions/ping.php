34d9dded0a2b8e9f779f58ab79f131c13d0d9800
Mikkel Jakobsen <mikkel@adapt.dk>
2014-06-06 09:45:34 +0000

    Correcting wrong syntax and documentation

--- scripts/ftp_permissions/ping.php
+++ scripts/ftp_permissions/ping.php
@@ -2,10 +2,9 @@
 #!/usr/bin/env drush
 
 // Example of execution:
-// drush scr --root=/home/drupal/kms.live/site/htdocs /home/drupal/kms.live/site/scripts/ftp_permissions/ping.php
+// drush scr --root=/home/drupal/kms.test/site/htdocs ping.php --script-path=/home/drupal/kms.test/site/scripts/ftp_permissions
 
-
-// check if we can bootstrap
+// Check if we can bootstrap.
 $self = drush_sitealias_get_record('@self');
 if (empty($self)) {
   drush_die("I can't bootstrap from the current location.", 0);
@@ -18,15 +17,20 @@ $ftp_script_hosts = array(
 // Run the ftp thing.
 kms_permissions_ftp_scan_store_structure();
 
-if (variable_get('kms_ftp_perm_change')){
+if (variable_get('kms_ftp_perm_change')) {
   $wd_msg = array();
-  //Trigger external script
-  foreach($ftp_script_hosts as $host) {
+  // Trigger external script.
+  foreach ($ftp_script_hosts as $host) {
     exec(sprintf('ssh -i  $HOME/.ssh/updateftpservers %s', $host), $message, $exit_code);
-    var_dump(sprintf('ssh -i  $HOME/.ssh/updateftpservers %s', $host));
     $wd_msg[] = $message;
   }
-  // Set variable to false, as script should revisit the view and update things accordingly
+  // Set variable to false,
+  // as script should revisit the view and update things accordingly.
   variable_set('kms_ftp_perm_change', FALSE);
-  watchdog('Permissions FTP', 'Ftp cron script ran with messages: @messages', array('@messages' => implode(', ' $wd_msg)), WATCHDOG_NOTICE);
+  watchdog(
+    'Permissions FTP',
+    'Ftp cron script ran with messages: @messages',
+    array('@messages' => implode(', ', $wd_msg)),
+    WATCHDOG_NOTICE
+  );
 }
