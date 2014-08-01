9a16889ecf35f26312b51658be264b4929014f92
Tom Helmer Hansen <tom@adapt.dk>
2014-08-01 13:15:41 +0000

    add csv input

--- htdocs/sites/all/modules/custom/kms_mapwidget/js/kms-mapwidget.js
+++ htdocs/sites/all/modules/custom/kms_mapwidget/js/kms-mapwidget.js
@@ -16,24 +16,29 @@ jQuery(document).ready(function($) {
   map = initMap();
   initSearch(map);
 
-
   $('#rect-button').click(function(e) {
-    $('a.toggle').removeClass('active');
+    $('.toggle').removeClass('active');
     $(this).addClass('active');
     if (selectCtrl) selectCtrl.activate();
     return false;
   });
 
   $('#select-button').click(function(e) {
-    $('a.toggle').removeClass('active');
+    $('.toggle').removeClass('active');
     $(this).addClass('active');
     if (highlightCtrl) highlightCtrl.activate();
     if (selectCtrl) selectCtrl.activate();
     return false;
   });
 
+  $('#csv-button').click(function(e) {
+    $('.toggle').removeClass('active');
+    $(this).parent().addClass('active');
+    return false;
+  });
+
   $('#drag-button').click(function(e) {
-    $('a.toggle').removeClass('active');
+    $('.toggle').removeClass('active');
     $(this).addClass('active');
     if (highlightCtrl) highlightCtrl.deactivate();
     if (selectCtrl) selectCtrl.deactivate();
@@ -41,7 +46,7 @@ jQuery(document).ready(function($) {
   });
 
   $('#reset-button').click(function(event) {
-    
+
     if (conf.type == 'drawrect') {
       if (selectLayer.features.length > 0) {
         selectLayer.removeFeatures(selectLayer.features[0]);
@@ -64,6 +69,7 @@ jQuery(document).ready(function($) {
   $('li.rect').hide();
   $('li.select').hide();
   $('li.reset').hide();
+  $('li.csv').hide();
 
   $.fn.updateForm = function(data) {};
 
@@ -85,7 +91,7 @@ jQuery(document).ready(function($) {
 	       e.preventDefault();
 	    });
     }
-    
+
     $('div.product-body').hide();
     $('div.useful-links').hide();
     $('#selection_message').show();
@@ -101,7 +107,7 @@ jQuery(document).ready(function($) {
         map.removeControl(highlightCtrl);
         highlightCtrl = false;
       }
-      
+
       if (selectLayer) {
         map.removeLayer(selectLayer);
         selectLayer = false;
@@ -113,16 +119,16 @@ jQuery(document).ready(function($) {
       }
 
       switch (conf.type) {
-        case '10kmgrid' : 
+        case '10kmgrid' :
           addGrid(map,conf);
           break;
         case 'dagi' :
           addDagi(map,conf);
-          break;  
-        case 'onefile' : 
+          break;
+        case 'onefile' :
           addFile(map,conf);
           break;
-        case 'drawrect' : 
+        case 'drawrect' :
           addDrawRect(map,conf);
           break;
       }
@@ -137,15 +143,15 @@ jQuery(document).ready(function($) {
       selectLayer.setVisibility(false);
     }
 
-    if (bboxLayer) 
+    if (bboxLayer)
       bboxLayer.setVisibility(true);
 
     if ( polygonLayer ) {
       polygonLayer.setVisibility(true);
     }
-    
+
     switch (conf.type) {
-        case '10kmgrid' : 
+        case '10kmgrid' :
           $('li.select').show();
           $('li.reset').show();
           $('#select-button').click();
@@ -155,13 +161,14 @@ jQuery(document).ready(function($) {
         case 'dagi' :
           $('li.select').show();
           $('li.reset').show();
+          $('li.csv').show();
           $('#select-button').click();
           $('.button-submit').attr('disabled', 'disabled');
           $('.button-submit').css('background-color','#EEEEEE');
-          break;  
+          break;
         case 'onefile' :
           break;
-        case 'drawrect' : 
+        case 'drawrect' :
           $('li.rect').show();
           $('li.reset').show();
           $('#rect-button').click();
@@ -175,7 +182,7 @@ jQuery(document).ready(function($) {
     jQuery.fn.updateForm();
 
   }
-})(jQuery);  
+})(jQuery);
 
 (function($) {
   $.fn.resetSelection = function(data) {
@@ -195,6 +202,7 @@ jQuery(document).ready(function($) {
   $('li.rect').hide();
   $('li.select').hide();
   $('li.reset').hide();
+  $('li.csv').hide();
   $('#drag-button').click();
 
   if ( selectLayer )
@@ -202,15 +210,14 @@ jQuery(document).ready(function($) {
 
   if ( bboxLayer )
     bboxLayer.setVisibility(false);
-  
   }
-})(jQuery);  
+})(jQuery);
 
 function initMap() {
-  
+
   OpenLayers.ImgPath = "/sites/all/themes/custom/kms/images/";
 
-  kmsticket: '',  
+  //kmsticket: '',
   kmsticket = new VisStedet.Ticket();
 
   // --------------------------------------------------------------------
@@ -231,7 +238,7 @@ function initMap() {
 
   // The resolutions - changes should only be done according to the tiling schemas
   var olResolutions = new Array(0.8,1.6,3.2,6.4,12.8,25.6,51.2,102.4,204.8,409.6,819.2,1638.4);
-  
+
   var skaermkort_url = ["http://a.kortforsyningen.kms.dk/topo_skaermkort",
                         "http://b.kortforsyningen.kms.dk/topo_skaermkort",
                         "http://c.kortforsyningen.kms.dk/topo_skaermkort",
@@ -263,10 +270,10 @@ function initMap() {
 			style: "default",
 			opacity: 1.0,
 			isBaselayer: true,
-			params: { ticket: kmsticket }  
+			params: { ticket: kmsticket }
 		}
 	);
-  
+
   if (conf.overlay) {
     var layers = conf.service_layer.split(',');
     for (var i = 0; i < layers.length; i++) {
@@ -274,15 +281,15 @@ function initMap() {
       overlay = new OpenLayers.Layer.WMS (
         layername,
         "http://kortforsyningen.kms.dk/" + conf.service_name + '?',
-        { layers: layername, 
-          format: 'image/png', 
+        { layers: layername,
+          format: 'image/png',
           bgcolor: '0xFFFFFF',
           ticket: kmsticket,
           transparent: true
         },
-        { singleTile: false, 
-          transitionEffect: 'resize', 
-          buffer: 0, 
+        { singleTile: false,
+          transitionEffect: 'resize',
+          buffer: 0,
           isBaseLayer : false,
           visibility: i == 0 ? true : false
         }
@@ -295,15 +302,15 @@ function initMap() {
     baselayer_specific = new OpenLayers.Layer.WMS (
   		conf.name,
   		"http://kortforsyningen.kms.dk/" + conf.service_name + '?',
-  		{ layers: conf.service_layer, 
-  		  format: 'image/png', 
+  		{ layers: conf.service_layer,
+  		  format: 'image/png',
   		  bgcolor: '0xFFFFFF',
   		  ticket: kmsticket,
   		  transparent: true
   		},
-  		{ singleTile: false, 
-  		  transitionEffect: 'resize', 
-  		  buffer: 0, 
+  		{ singleTile: false,
+  		  transitionEffect: 'resize',
+  		  buffer: 0,
   		  isBaseLayer : true
   		}
   	);
@@ -322,10 +329,10 @@ function initMap() {
   // Layer selector in upper right corner. (Base layers show as choices between
   // radio buttons, other layers as selectable checkboxes):
   map.addControl(new OpenLayers.Control.LayerSwitcher( {title:Drupal.t('Select layer')} ));
-	
-  // Set center and zoom 
+
+  // Set center and zoom
   map.setCenter(new OpenLayers.LonLat(conf.center_longitude, conf.center_latitude),conf.zoom_level);
- 
+
   // Set the layerswitcher baselayer / overlay labels
   layerSwitcher = map.getControlsByClass("OpenLayers.Control.LayerSwitcher")[0];
   layerSwitcher.baseLbl.innerText = "Basiskort";
@@ -336,7 +343,7 @@ function initMap() {
 }
 
 function addFile(map,conf) {
-    
+
   (function($) {
 
     var files = Array();
@@ -344,10 +351,10 @@ function addFile(map,conf) {
     $('#selection_message').append('<p>' + Drupal.t('Selection method') + ' : <span class="value">' + conf.product_type_title + ' - ' + conf.selection_type_title + '</span></p>');
     $('#selection_message').append('<p>' + Drupal.t('Dataformat') + ' :<span class="value"> ' + conf.dataformat_title + '</span></p>');
     $('#selection_message').append('<p>' + Drupal.t('Reference system') + ' : <span class="value">' + conf.koordinatsystem_title + '</span></p>');
-    $('#selection_message').append('<h3>' + Drupal.t('Selected items') + '</h3>'); 
+    $('#selection_message').append('<h3>' + Drupal.t('Selected items') + '</h3>');
     $('#selection_message').append('<ul></ul');
-    
-    if (conf.product_type == 'predefined') 
+
+    if (conf.product_type == 'predefined')
       file = conf.details;
     else
       file = Drupal.t('Country-wide file');
@@ -382,9 +389,9 @@ function addGrid(map,conf) {
 
   selectCtrl = new OpenLayers.Control.SelectFeature(selectLayer,
     { box: true,
-      clickout: false,  
+      clickout: false,
       toggle: true,
-      multiple: true, 
+      multiple: true,
       onSelect:selected,
       onUnselect:unselected
     }
@@ -397,13 +404,13 @@ function addGrid(map,conf) {
    });
 
    // This sould be set to make mouse pan work
-   //selectCtrl.handlers.feature.stopDown = false;  
+   //selectCtrl.handlers.feature.stopDown = false;
    //highlightCtrl.handlers.feature.stopDown = false;
 
   map.addControl(highlightCtrl);
   map.addControl(selectCtrl);
 
-  highlightCtrl.activate();   
+  highlightCtrl.activate();
   selectCtrl.activate();
 
   jQuery.getJSON(conf.grid_folder + conf.details,
@@ -430,14 +437,14 @@ function addGrid(map,conf) {
     $('#selection_message').append('<p>' + Drupal.t('Selection method') + ' :<span class="value"> ' + conf.product_type_title + ' - ' + conf.selection_type_title + '</span></p>');
     $('#selection_message').append('<p>' + Drupal.t('Dataformat') + ' :<span class="value"> ' + conf.dataformat_title + '</span></p>');
     $('#selection_message').append('<p>' + Drupal.t('Reference system') + ' :<span class="value"> ' + conf.koordinatsystem_title + '</span></p>');
-    $('#selection_message').append('<h3>' + Drupal.t('Selected items') + '</h3>'); 
+    $('#selection_message').append('<h3>' + Drupal.t('Selected items') + '</h3>');
     jQuery('#selection_message').append('<ul></ul');
-    
+
     for (var i=0; i<selectLayer.selectedFeatures.length; i++) {
-      
+
       var filename;
 
-      if (conf.product_type == 'predefined') 
+      if (conf.product_type == 'predefined')
         filename = selectLayer.selectedFeatures[i].attributes.filename;
       else
         filename = selectLayer.selectedFeatures[i].attributes.fileid;
@@ -514,7 +521,7 @@ if (conf.details == 'kommune.json' ) {
        "select":new OpenLayers.Style({'fillColor' : '#00FF00', 'fillOpacity' : 0.2}),
        "highlight":new OpenLayers.Style({'fillColor' : '#0000FF', 'fillOpacity' : 0.1})
   });
-  
+
   selectLayer = new OpenLayers.Layer.Vector('DAGI', {
     'displayInLayerSwitcher' : false,
     'styleMap' : styleMap
@@ -524,9 +531,9 @@ if (conf.details == 'kommune.json' ) {
 
   selectCtrl = new OpenLayers.Control.SelectFeature(selectLayer,
       { box: true,
-        clickout: false,  
+        clickout: false,
       toggle: true,
-      multiple: true, 
+      multiple: true,
       onSelect:selected,
       onUnselect:unselected
     }
@@ -542,22 +549,22 @@ if (conf.details == 'kommune.json' ) {
    map.addControl(selectCtrl);
 
    highlightCtrl.activate();
-   selectCtrl.activate();   
+   selectCtrl.activate();
 
   if (conf.details == 'ejerlav.json' ) {
      map.events.register('zoomend', this, function(event) {
-       if (map.getZoom() > 3) 
+       if (map.getZoom() > 3)
          selectLayer.setVisibility(true);
-       else 
+       else
          selectLayer.setVisibility(false);
     });
   }
 
   if (conf.details == 'sogn.json' ) {
      map.events.register('zoomend', this, function(event) {
-       if (map.getZoom() > 2) 
+       if (map.getZoom() > 2)
          selectLayer.setVisibility(true);
-       else 
+       else
          selectLayer.setVisibility(false);
     });
   }
@@ -576,9 +583,9 @@ if (conf.details == 'kommune.json' ) {
   /* Bounding box layer */
 
   if (conf.product_type == 'userdefined') {
-    var bboxStyleMap = new OpenLayers.StyleMap( { 
-      'default':new OpenLayers.Style({ 
-        'strokeColor' : '#97bc00', 
+    var bboxStyleMap = new OpenLayers.StyleMap( {
+      'default':new OpenLayers.Style({
+        'strokeColor' : '#97bc00',
         'strokeWidth' : 5,
         'fillOpacity' : 0
     })});
@@ -598,6 +605,28 @@ if (conf.details == 'kommune.json' ) {
     jQuery.fn.updateForm();
   }
 
+  /* CSV selection handler */
+  (function($) {
+    $("#update-from-csv").click(function(e) {
+      $('#selection_errors').empty();
+      selectCtrl.unselectAll();
+      var codes = $('textarea#csv-data').val().split(',');
+      codes = sort_unique(codes);
+      codeVarName = getCodeVarName(conf);
+      for(var f=0; f<codes.length; f++) {
+        code = codes[f].trim();
+        feature = selectLayer.getFeaturesByAttribute(codeVarName, code);
+        if (feature.length == 1) {
+          selectCtrl.select(feature[0]);
+        } else {
+          $('#selection_errors').append('<p style="color:red">"' + code + '" ' + Drupal.t('is not a valid code') + '</p>');
+        }
+      }
+      e.preventDefault();
+
+    });
+  })(jQuery);
+
 (function($) {
   $.fn.updateForm = function(data) {
 
@@ -613,13 +642,13 @@ if (conf.details == 'kommune.json' ) {
     if ( conf.product_type == 'userdefined' ) {
       $('#selection_message').append('<p>' + Drupal.t('Maximum allowed area') + ' :<span class="value">' + conf.max_area + ' km&sup2;</span></p>');
     }
-    $('#selection_message').append('<h3>' + Drupal.t('Selected items') + '</h3>'); 
+    $('#selection_message').append('<h3>' + Drupal.t('Selected items') + '</h3>');
     $('#selection_message').append('<ul></ul');
 
     for (var i=0; i<selectLayer.selectedFeatures.length; i++) {
-        
+
         var feature = selectLayer.selectedFeatures[i];
-        
+
         if (feature.geometry.bounds.bottom < bounds.bottom)
           bounds.bottom = feature.geometry.bounds.bottom;
 
@@ -672,7 +701,7 @@ if (conf.details == 'kommune.json' ) {
         var geometry = bounds.toGeometry();
         var area = geometry.getArea() / 1000000;
         $('#selection_message ul').prepend('<p>' + Drupal.t('Area') + ' : <span class="value">' + area.toFixed(2).replace('.',',') + ' km&sup2;</span></p');
-        
+
         /* Draw bounding box rectangle */
         var rectangle = new OpenLayers.Feature.Vector(geometry);
         bboxLayer.addFeatures(rectangle);
@@ -689,17 +718,38 @@ if (conf.details == 'kommune.json' ) {
       } else {
         $('.button-submit').removeAttr('disabled');
         $('.button-submit').css('background-color','#97bc00');
-      }  
+      }
     } else {
       $('.button-submit').css('background','#EEEEEE');
       $('.button-submit').attr('disabled', 'disabled');
     }
-    
+
     $('input[name=selection]').val(JSON.stringify(files));
 
   };
 })(jQuery);
 
+}
+
+function sort_unique(arr) {
+    arr = arr.sort(function (a, b) { return a*1 - b*1; });
+    var ret = [arr[0]];
+    for (var i = 1; i < arr.length; i++) { // start loop at 1 as element 0 can never be a duplicate
+        if (arr[i-1] !== arr[i]) {
+            ret.push(arr[i]);
+        }
+    }
+    return ret;
+}
+
+function getCodeVarName(conf) {
+  codenames= {
+    'region.json': 'REGIONKODE',
+    'kommune.json': 'KOMKODE',
+    'ejerlav.json': 'ELAVSKODE',
+    'sogn.json': 'SOGNEKODE'
+  };
+  return codenames[conf.details];
 }
 
 function addDrawRect(map,conf) {
