7d76aebcbb1a8bf4e4976762cba6a3625e1c8132
Tom Helmer Hansen <tom@adapt.dk>
2014-08-05 10:24:14 +0000

    extra closing div deleted

--- htdocs/sites/all/themes/custom/kms/templates/page.tpl.php
+++ htdocs/sites/all/themes/custom/kms/templates/page.tpl.php
@@ -1,4 +1,4 @@
-<div class="admin-tabs"><?php if ($tabs): ?><?php print render($tabs); ?></div><?php endif; ?></div>
+<div class="admin-tabs"><?php if ($tabs): ?><?php print render($tabs); ?><?php endif; ?></div>
 <div id="page"><?php print render($page['content']) ?></div>
 <!-- Site Improve -->
 <script type="text/javascript" src="//ssl.siteimprove.com/js/siteanalyze_133886.js">/**/</script>
