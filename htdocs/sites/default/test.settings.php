8e5bf9997c0bb1f6ed7d28d891bca15003c404cb
Mikkel Jakobsen <mikkel@adapt.dk>
2014-08-29 09:02:03 +0000

    Adding base_url to test as well

--- htdocs/sites/default/test.settings.php
+++ htdocs/sites/default/test.settings.php
@@ -274,7 +274,7 @@ $drupal_hash_salt = '';
  * It is not allowed to have a trailing slash; Drupal will add it
  * for you.
  */
-# $base_url = 'http://www.example.com';  // NO trailing slash!
+ $base_url = 'http://test.download.kortforsyningen.dk';  // NO trailing slash!
 
 /**
  * PHP settings:
