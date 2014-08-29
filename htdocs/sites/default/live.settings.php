68b7276a05d516ed040bf2d316e6854b63e8f361
Mikkel Jakobsen <mikkel@adapt.dk>
2014-08-29 07:41:36 +0000

    Hardcoding base_url in order to prevent DNS failures

--- htdocs/sites/default/live.settings.php
+++ htdocs/sites/default/live.settings.php
@@ -296,7 +296,7 @@ $drupal_hash_salt = '';
  * It is not allowed to have a trailing slash; Drupal will add it
  * for you.
  */
-# $base_url = 'http://www.example.com';  // NO trailing slash!
+ $base_url = 'http://download.kortforsyningen.dk';  // NO trailing slash!
 
 /**
  * PHP settings:
