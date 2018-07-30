d39b74bfb5de844a4fe1cd905002d42852d649f7
Marcin Slezak <mars@mixpoint.dk>
2018-07-30 13:45:00 +0000

    Fix JS issues when submitting

--- modules/custom/kms_user/js/gdpr_scroll.js
+++ modules/custom/kms_user/js/gdpr_scroll.js
@@ -1,8 +1,24 @@
 (function ($) {
   Drupal.behaviors.kms_user_gdpr_scroll = {
     attach: function (context, settings) {
+      // Make sure that the consent body is read before you can accept.
+      $('#consent-body').scroll(function () {
+        var hT = $('#scroll-to').offset().top,
+            hH = $('#scroll-to').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
 
-      console.log('GDPR');
+        if ((wS - 20) > (hT + hH - wH)) {
+          $('#edit-consent-check').removeAttr('disabled');
+        }
+      });
+
+      // When the consent check is clicked, we can enable the continue button.
+      $('#edit-consent-check').change(function () {
+        if ($('#edit-consent-check').is(':checked') == true) {
+          $('#edit-submit').removeAttr('disabled');
+        }
+      });
 
       $('#terms-body').scroll(function () {
         var hT = $('#scroll-to').offset().top,
@@ -11,8 +27,13 @@
             wS = $(this).scrollTop();
 
         if ((wS - 20) > (hT + hH - wH)) {
-          $('#edit-submit').removeAttr('disabled');
           $('#term-check').removeAttr('disabled');
+        }
+      });
+
+      $('#term-check').change(function () {
+        if ($('#edit-consent-check').is(':checked') == true) {
+          $('#edit-submit').removeAttr('disabled');
         }
       });
     }
